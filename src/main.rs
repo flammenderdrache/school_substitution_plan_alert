392e6d55a255d6c6780ce46ae1b824c21a751b21
xUnixorn <jonas.crull@gmx.de>
2021-08-08 05:22:35 +0200

    changed return type of main to result

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,9 @@ use crate::substitution_schedule::SubstitutionSchedule;
 mod substitution_schedule;
 mod tabula_json_parser;
 
-fn main() {
-	let mut text_as_vec = match tabula_json_parser::parse(File::open("tabula/1337.json").unwrap()) {
-		Ok(str) => { str }
-		Err(why) => { panic!("{}", why) }
-	};
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+	let substitutions = SubstitutionSchedule::from_pdf("tabula/42069");
+	println!("{}", substitutions?);
 
-	let substitutions = SubstitutionSchedule::from_csv(&mut text_as_vec);
-	println!("{}", substitutions);
+	Ok(())
 }
