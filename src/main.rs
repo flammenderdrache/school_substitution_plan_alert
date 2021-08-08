f402f61ee43cb01b6c69ae1a931ae47a5362e48b
flammenderdrache <laberschleichi@gmail.com>
2021-08-08 04:15:19 +0200

    Fix compile error

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,10 @@ mod jonas;
 mod tabula_json_parser;
 
 fn main() {
-	let mut text_as_vec = match tabula_json_parser::parse_file(File::open("tabula/1337.json").unwrap()) {
+	let mut text_as_vec = match tabula_json_parser::parse(File::open("tabula/1337.json").unwrap()) {
 		Ok(str) => { str }
 		Err(why) => { panic!("{}", why) }
 	};
-
-	println!("{}", json);
 
 	let substitutions = SubstitutionSchedule::from_csv(&mut text_as_vec);
 	println!("{}", substitutions);
